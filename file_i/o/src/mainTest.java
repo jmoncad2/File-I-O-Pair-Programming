436b8576924ed718efd1960df9e2237cca4959a5
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-25 14:35:58 -0500

    Added skeleton code

---
+++ file_i/o/src/mainTest.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class mainTest {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    }
+}
---
+++ file_i/o/src/main_io.java
@@ -0,0 +1,28 @@
+
+
+public class main_io {
+    String fileLocation;
+    String name;
+    Integer phone;
+
+    public main_io(){
+        this.fileLocation = fileLocation;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getLocation(){
+        return fileLocation;
+    }
+
+    public Integer getPhone(){
+        return this.phone;
+    }
+
+
+
+}
