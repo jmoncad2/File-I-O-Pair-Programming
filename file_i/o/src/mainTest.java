849fc74dcd2e038bc6ab70b9d3beb97fc9883200
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-25 14:45:41 -0500

    Added code for new contacts.txt file when code runs

--- file_i/o/src/mainTest.java
+++ file_i/o/src/mainTest.java
@@ -1,9 +1,27 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class mainTest {
-    public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+
+        if (Files.notExists(dataDirectory)){
+            Files.createDirectories(dataDirectory);
+        }
+        if (! Files.exists(dataFile)){
+            Files.createFile(dataFile);
+        }
+
 
     }
 }
