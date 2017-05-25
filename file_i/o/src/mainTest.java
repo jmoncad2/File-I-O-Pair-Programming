2272c32bfb0d27c5702346ff8a0ade755e752ba8
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-25 15:07:39 -0500

    Added ablility to see the file and some more functionality

--- file_i/o/src/mainTest.java
+++ file_i/o/src/mainTest.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -21,7 +22,46 @@ public class mainTest {
         if (! Files.exists(dataFile)){
             Files.createFile(dataFile);
         }
+        displayMenu();
+        switch (options(input)){
+            case 1:
+                List<String> lines = Files.readAllLines(dataFile);
+                for(String line : lines){
+                    System.out.println(line);
+                }
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+                System.out.println("Good Bye :)");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Option; Please Choose Another");
+        }
 
 
     }
+
+    public static void displayMenu(){
+        System.out.println("+*+*+*+*+*PLEASE CHOOSE AN OPTION+*+*+*+*+*");
+        System.out.println("1) View contacts");
+        System.out.println("2) Add a new contact");
+        System.out.println("3) Search a contact by name");
+        System.out.println("4) Delete an existing contact");
+        System.out.println("5) Exit");
+    }
+
+    public static int options(Scanner input){
+        int option = input.nextInt();
+        return option;
+    }
+
 }
